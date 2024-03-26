8b4b851fd52c3e8f414db4d4d0371b31fe426b6c
AccelerateCryEngi <113775227+AccelerateCryEngi@users.noreply.github.com>
2024-03-26 09:15:49 +0300

    Add files via upload

---
+++ students.py
@@ -0,0 +1,21 @@
+with open('students.csv', encoding='utf-8') as infile:
+    a = infile.readlines()
+a.pop(0)
+for i in range(len(a)):
+    a[i] = a[i].strip().split(',')
+for note in a:
+    if 'Хадаров Владимир' in note[1]:
+        print('Ты получил: %s, за проект - %s' % (note[4], note[2]))
+d = {}
+for note in a:
+    if note[4] != 'None':
+        if note[3] in d:
+            d[note[3]][0] += 1
+            d[note[3]][1] += int(note[4])
+        else:
+            d[note[3]] = [1, int(note[4])]
+for note in d:
+    d[note] = round(d[note][1]/d[note][0], 3)
+for i in range(len(a)):
+    if a[i][4] == 'None':
+        a[i][4] = d[a[i][3]]
