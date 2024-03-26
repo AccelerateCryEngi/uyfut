4b810c49b0ece57e9a420ed840e06c4d9436060b
AccelerateCryEngi <113775227+AccelerateCryEngi@users.noreply.github.com>
2024-03-26 13:35:42 +0300

    Update students.py

--- students.py
+++ students.py
@@ -3,7 +3,7 @@ with open('students.csv', encoding='utf-8') as infile:
 a.pop(0)
 for i in range(len(a)):
     a[i] = a[i].strip().split(',')
-for note in a:
+    note = a[i]
     if 'Хадаров Владимир' in note[1]:
         print('Ты получил: %s, за проект - %s' % (note[4], note[2]))
 d = {}
@@ -19,3 +19,9 @@ for note in d:
 for i in range(len(a)):
     if a[i][4] == 'None':
         a[i][4] = d[a[i][3]]
+
+f = open('student_new.csv', 'w', encoding='utf-8')
+f.writelines('id,Name,titleProject_id,class,score\n')
+for x in a:
+    f.writelines(','.join(map(str, x))+'\n')
+f.close()
