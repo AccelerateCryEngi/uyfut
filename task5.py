0c23092209d108037f25cec2be1171d48da0fcc8
Student <officialcryengi@gmail.com>
2024-03-29 12:22:32 +0300

    Added tab

--- task5.py
+++ task5.py
@@ -1,26 +1,25 @@
-with open('students.csv', encoding='utf-8') as f:
-    a = f.readlines()
-title = a.pop(0)
-slovar = {x: y + 1 for y, x in enumerate('абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ ')}
-
-
-def hash(string):
-    p = 67
+def hash(s):
+    s = s[1]
+    p= 67
     m = 10**9 + 9
-    res = []
-    for i in range(len(string)):
-        res.append(string[0]*p**i%m)
-    return res
+    h=0
 
+    for i in range(len(s)):
+
+        h += A.find(s[i])*p**i
+    h = h%m
+    return str(h)
+
+A='АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+a = A.lower()
+A = '*'+ A + a + ' '
+a=open('students.csv', encoding='utf8').readlines()
+shapka = a.pop(0)
 for i in range(len(a)):
-    item = a[i].split(',')
-    fio = item[1]
-    prehash = [slovar[x] for x in fio]
-    hashed = hash(prehash)
-    a[i] = [''.join(map(str, hashed))]+a[i].split(',')[1:]
+    a[i] =a[i].strip().split(',')
 
-with open('students_with_hash.csv', 'w') as f:
-    f.write(title)
-    for item in a:
-        f.write('\n')
-        f.write(','.join(item))
+f = open('students_with_hash.csv', 'w', encoding='utf8')
+f.write(shapka)
+for x in a:
+    x[0] = hash(x)
+    f.write(','.join(x) + '\n')
